5de1a40e0ed4d2a3d1ed753d12456193f0a2ecae
csquaresanthosh <csquaresanthosh@gmail.com>
2024-07-20 09:53:52 +0530

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Second</title>
+    
+  </head>
+  <body>
+    <br><br><br><br><br><br>
+    <center>
+      <h1>
+        <?php 
+          echo "Santhosh";
+        ?>
+      </h1>
+    </center>
+  </body>
+</html>
